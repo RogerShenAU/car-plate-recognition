3dedc6ed5a8116204a0f0424ca568412ecb6b308
rogershenau <roger.shen.au@gmail.com>
2018-07-17 10:53:07 +1000

    convert to lazy loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+@IonicPage({
+  name: 'HomePage'
+})
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
 
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
   }
 
 }
